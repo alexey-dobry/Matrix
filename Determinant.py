21cd79551b20cc0f0315f665e99ea03345b05251
Alexey Nehaenko <Nehaenkog23@gmail.com>
2023-09-16 22:25:47 +0500

    rewrote Determinant.py file

--- Determinant.py
+++ Determinant.py
@@ -13,29 +13,33 @@ class Determinant:
         if obj.stroka == 1:
             return obj.matrix['11']
         elif obj.stroka == 2:
-            return obj.find_2_det(obj)
+            return obj.det_2(obj)
         else:
-            return obj.find_n_det(obj)
+            return obj.det_n(obj)
 
     @staticmethod
-    def find_2_det(obj):
+    def det_2(obj):
         return obj.matrix['11'] * obj.matrix['22'] - obj.matrix['12'] * obj.matrix['21']
 
     @staticmethod
-    def find_n_det(obj):
+    def det_n(obj):
         __value_of_det = 0
-        for coefficient_pozition in range(1,obj.stroka + 1):
-            new_matrix_values = []
-            stroki_new_det = ''.join([str(x) for x in range(1,obj.stroka + 1) if x != coefficient_pozition])
-            stolbs_new_det = ''.join([str(x) for x in range(2,obj.stroka + 1)])
-            for i in stroki_new_det:
-                for j in stolbs_new_det:
-                    new_matrix_values.append(obj.matrix[str(i) + str(j)])
-            Minor = Determinant(Matrix(new_matrix_values,obj.stroka - 1,obj.stroka - 1))
-            Delta_det = (-1)**(coefficient_pozition + 1)*obj.matrix[str(coefficient_pozition) + '1']* Minor.find_det
-            __value_of_det += Delta_det
+        for coefficient_position in range(1,obj.stroka + 1):
+            Minor = obj.find_Minor(obj,coefficient_position)
+            Algebraic_complement = (-1)**(coefficient_position + 1)*Minor.find_det
+            Coefficent = obj.matrix[str(coefficient_position) + '1']
+            __value_of_det += Coefficent*Algebraic_complement
         return __value_of_det
 
+    @staticmethod
+    def find_Minor(obj,coefficient_position):
+        new_matrix_values = []
+        stroki_new_det = ''.join([str(x) for x in range(1, obj.stroka + 1) if x != coefficient_position])
+        stolbs_new_det = ''.join([str(x) for x in range(2, obj.stroka + 1)])
+        for i in stroki_new_det:
+            for j in stolbs_new_det:
+                new_matrix_values.append(obj.matrix[str(i) + str(j)])
+        return Determinant(Matrix(new_matrix_values,obj.stroka - 1,obj.stroka - 1))
     @staticmethod
     def check_for_square(m,n):
         if m != n: raise MatrixNotSquareError
